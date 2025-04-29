e2d47f0c5303134c50ace0aa599bdefe3d180a66
Guilherme Cittadela <guicittadela@hotmail.com>
2025-04-29 13:31:29 -0300

    criacao dos repositorios para insercao no banco

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,8 +1,39 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositoUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID inserido: %d", usuarioID)))
 
 }
 
