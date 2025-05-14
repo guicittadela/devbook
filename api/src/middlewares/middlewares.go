c2a5cffa0b087ac89d3440af066eec22dc49e739
Guilherme Cittadela <guicittadela@hotmail.com>
2025-05-14 19:23:03 -0300

    implementacao de login via token

--- api/src/middlewares/middlewares.go
+++ api/src/middlewares/middlewares.go
@@ -1,20 +1,23 @@
 package middlewares
 
 import (
-	"fmt"
-	"log"
+	"api/src/autenticacao"
+	"api/src/respostas"
 	"net/http"
 )
 
 func Logger(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("\n %s %s %s ", r.Method, r.RequestURI, r.Host)
+		//log.Printf("\n %s %s %s ", r.Method, r.RequestURI, r.Host)
 		next(w, r)
 	}
 }
 func Autenticar(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("validand")
+		if erro := autenticacao.ValidarToken(r); erro != nil {
+			respostas.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
 		next(w, r)
 	}
 }
