16e75172d2e403a804c8f464fe212e8ff2cd03d5
Guilherme Cittadela <guicittadela@hotmail.com>
2025-05-27 23:54:47 -0300

    implementacao de renderizaocao na pagina principal

--- web/src/controllers/paginas.go
+++ web/src/controllers/paginas.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"web/src/config"
+	"web/src/modelos"
 	"web/src/requisicoes"
+	"web/src/respostas"
 	"web/src/utils"
 )
 
@@ -20,6 +23,22 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 	response, erro := requisicoes.RequisicaoComAutenticacao(r, http.MethodGet, url, nil)
 
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{ErroAPI: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{ErroAPI: erro.Error()})
+		return
+	}
 	fmt.Println(response.StatusCode, erro)
-	utils.ExecutarTemplate(w, "home.html", nil)
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
