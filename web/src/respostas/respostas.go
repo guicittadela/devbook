46acc4af14adade952ad23ea15264be5cb19e623
Guilherme Cittadela <guicittadela@hotmail.com>
2025-05-30 11:53:14 -0300

    implementacao de curtir e descurtir publicacao

--- web/src/respostas/respostas.go
+++ web/src/respostas/respostas.go
@@ -14,8 +14,10 @@ func JSON(w http.ResponseWriter, statusCode int, dados any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if statusCode != http.StatusNoContent {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
 }
 
